2fd8d53d3ec86dd8b34bf9bff53b179567df3a37
kangyao.ma@sap.com <kangyao.ma@sap.com>
2021-07-20 18:58:06 +0800

    init kubebuilderht

---
+++ kubebuilderht/api/v1/hellotype_types.go
@@ -0,0 +1,65 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// HelloTypeSpec defines the desired state of HelloType
+type HelloTypeSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Message is an example field of HelloType. Edit hellotype_types.go to remove/update
+	Message string `json:"message,omitempty"`
+}
+
+// HelloTypeStatus defines the observed state of HelloType
+type HelloTypeStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	Name string `json:"name,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// HelloType is the Schema for the hellotypes API
+type HelloType struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   HelloTypeSpec   `json:"spec,omitempty"`
+	Status HelloTypeStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// HelloTypeList contains a list of HelloType
+type HelloTypeList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []HelloType `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&HelloType{}, &HelloTypeList{})
+}
