569ed9005f48588c96c188bf3ec19d7f55a05e86
kangyao.ma@sap.com <kangyao.ma@sap.com>
2021-09-24 14:07:10 +0800

    do some modification

--- ptdemo/main.go
+++ ptdemo/main.go
@@ -7,15 +7,30 @@ import (
 )
 
 func main() {
-	f := os.Stdout
+	f, err := os.Create("./temp.txt")
 	search := pt.PlatinumSearcher{
 		Err: os.Stderr,
 		Out: f,
 	}
-	search.Run([]string{"-c", "k*", "/Users/i517131/go/src/typedclientdemo"})
+	search.Run([]string{"-c", "kube", "/Users/i517131/go/src/typedclientdemo"})
 	finfo, err := f.Stat()
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Println(finfo.Size())
+
+	strs := []string{"os", "kube"}
+	for _, str := range strs {
+		f, err := os.Create("temp.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		search := pt.PlatinumSearcher{
+			Err: os.Stderr,
+			Out: f,
+		}
+		search.Run([]string{"-c", str, "/Users/i517131/go/src/typedclientdemo"})
+		finfo, err := f.Stat()
+		log.Println(finfo.Size())
+	}
 }
