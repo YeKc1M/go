f869866c5e26232fe99d6905b410517938c5601c
kangyao.ma@sap.com <kangyao.ma@sap.com>
2021-10-08 11:12:29 +0800

    add defer

--- ptdemo/main.go
+++ ptdemo/main.go
@@ -8,18 +8,24 @@ import (
 
 func main() {
 	f, err := os.Create("./temp.txt")
+	defer func() {
+		err := os.Remove("./temp.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 	search := pt.PlatinumSearcher{
 		Err: os.Stderr,
 		Out: f,
 	}
-	search.Run([]string{"-c", "kube", "/Users/i517131/go/src/typedclientdemo"})
+	search.Run([]string{"-c", "-e", "(kube)|(os)", "/Users/i517131/go/src/typedclientdemo"})
 	finfo, err := f.Stat()
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Println(finfo.Size())
 
-	strs := []string{"os", "kube"}
+	strs := []string{"os", "kube", "231456"}
 	for _, str := range strs {
 		f, err := os.Create("temp.txt")
 		if err != nil {
