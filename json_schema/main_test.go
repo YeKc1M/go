1f3943e53d514947edebc389a0406dbae92da2ec
15008181891 <383250208@qq.com>
2021-11-17 22:53:03 +0800

    add test

---
+++ json_schema/main_test.go
@@ -0,0 +1,80 @@
+package json_schema
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/xeipuuv/gojsonschema"
+	"testing"
+)
+
+func Test_string(t *testing.T) {
+	schema_str := `{
+		"type":"object",
+		"properties":{
+			"name":{
+				"type":"string"
+			}
+		},
+		"required": ["name"]
+	}`
+	schema := gojsonschema.NewStringLoader(schema_str)
+	json_str := `{}`
+	json := gojsonschema.NewStringLoader(json_str)
+	res, err := gojsonschema.Validate(schema, json)
+	assert.Nil(t, err)
+	assert.False(t, res.Valid())
+
+	json_str = `{"age":123}`
+	json = gojsonschema.NewStringLoader(json_str)
+	res, err = gojsonschema.Validate(schema, json)
+	assert.Nil(t, err)
+	assert.False(t, res.Valid())
+
+	json_str = `{"name":123}`
+	json = gojsonschema.NewStringLoader(json_str)
+	res, err = gojsonschema.Validate(schema, json)
+	assert.Nil(t, err)
+	assert.False(t, res.Valid())
+
+	json_str = `{"name":"string"}`
+	json = gojsonschema.NewStringLoader(json_str)
+	res, err = gojsonschema.Validate(schema, json)
+	assert.Nil(t, err)
+	assert.True(t, res.Valid())
+}
+
+func Test_map(t *testing.T) {
+	m := map[string]interface{}{
+		"type":       "object",
+		"properties": map[string]interface{}{"name": "string"},
+		"required":   []string{"name"},
+	}
+	schema := gojsonschema.NewGoLoader(m)
+	json_map := map[string]interface{}{}
+	json := gojsonschema.NewGoLoader(json_map)
+	res, err := gojsonschema.Validate(schema, json)
+	assert.Nil(t, err)
+	assert.False(t, res.Valid())
+
+	json_map = map[string]interface{}{
+		"age": 123,
+	}
+	json = gojsonschema.NewGoLoader(json_map)
+	res, err = gojsonschema.Validate(schema, json)
+	assert.Nil(t, err)
+	assert.False(t, res.Valid())
+
+	json_map = map[string]interface{}{
+		"name": 123,
+	}
+	json = gojsonschema.NewGoLoader(json_map)
+	assert.Nil(t, err)
+	assert.False(t, res.Valid())
+
+	json_map = map[string]interface{}{
+		"name": "123",
+	}
+	json = gojsonschema.NewGoLoader(json_map)
+	res, err = gojsonschema.Validate(schema, json)
+	assert.Nil(t, err)
+	assert.True(t, res.Valid())
+}
